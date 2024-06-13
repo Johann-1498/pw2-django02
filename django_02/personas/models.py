5ba1362e2576c22f6439038f5bdb2d2f61e2be55
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-06-13 11:35:23 -0500

    Añdiendo mas vistas y analizando sus argumentos

--- django_02/personas/models.py
+++ django_02/personas/models.py
@@ -7,3 +7,5 @@ class Person(models.Model):
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
