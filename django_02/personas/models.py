091ef898137ad0cf6f77887d15f395ecbb1fcf02
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-06-13 11:33:06 -0500

    Personalizando pagina de inicio y registrando el url

--- django_02/personas/models.py
+++ django_02/personas/models.py
@@ -6,3 +6,4 @@ class Person(models.Model):
     age = models.IntegerField()
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
