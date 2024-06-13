aad267ef5df56baedd79327ce0b8298452419a85
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-06-13 11:37:29 -0500

    Creando templates,haciendo el uso de variables y diferentes tipos de tags

--- django_02/personas/models.py
+++ django_02/personas/models.py
@@ -6,6 +6,7 @@ class Person(models.Model):
     age = models.IntegerField()
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    correo = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.name
