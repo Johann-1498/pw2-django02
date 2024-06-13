f792fa2e9a6789f2077f55529202acf38ffc7164
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2024-06-13 11:31:15 -0500

    Cambiando modelo de personas

--- django_02/personas/models.py
+++ django_02/personas/models.py
@@ -5,3 +5,4 @@ class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
